a82b0979b20e3242839c19b9755f05e16a2f6847
REdo_o <edo-o@mail.ru>
2020-04-24 02:07:13 +0400

    added Splash screen added token verification logic in splash improved code

--- app/src/main/java/com/dalpak/bringit/LoginActivity.java
+++ app/src/main/java/com/dalpak/bringit/LoginActivity.java
@@ -40,7 +40,6 @@ public class LoginActivity extends AppCompatActivity {
 
          */
         initListener();
-        binding.tvGo.performClick(); // todo: remove before release
     }
 
     private void initListener() {
